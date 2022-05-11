859ca6d041dcce4c96e2698cafdcec269be47d01
The-ARJ <joshiaayush871@gmail.com>
2022-05-11 15:52:25 +0545

    Login- Backend Completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from .forms import CreateUserForm
-from django.contrib import messages
+from django.contrib import messages, auth
+
+
 # Create your views here.
 
 def signIn(request):
-    return render(request, 'accounts/signIn.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            if user.is_superuser and user.is_staff and user.is_active:
+                print("working")
+                return redirect('/admins')
+            elif not user.is_superuser and user.is_staff and user.is_active:
+                return redirect('/editors')
+            elif not user.is_superuser and not user.is_staff and user.is_active:
+                return redirect('/users')
+        else:
+            print("working")
+            messages.error(request, "Username or Password is incorrect.")
+            return render(request, 'accounts/signIn.html', {})
+    context = {}
+    return render(request, 'accounts/signIn.html', context)
+
 
 def signUp(request):
     if request.method == 'POST':
@@ -16,12 +40,12 @@ def signUp(request):
             userdata.save()
             messages.add_message(request, messages.SUCCESS, f'"{username}" registered successfully!!')
         else:
-            messages.add_message(request, messages.ERROR, 'Failed to register user!!')    
-            
-    context = {'form':CreateUserForm}
-    
+            messages.add_message(request, messages.ERROR, 'Failed to register user!!')
+
+    context = {'form': CreateUserForm}
+
     return render(request, 'accounts/signUp.html', context)
+
 
 def home(request):
     return render(request, 'accounts/home.html')
-    

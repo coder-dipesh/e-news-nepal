e9dd53d6b949d0ea2e95fa64ba0db7c7bf6d2b9e
The-ARJ <joshiaayush871@gmail.com>
2022-05-17 10:21:22 +0545

    Url Defined for Editor page and Register Page

--- admins/urls.py
+++ admins/urls.py
@@ -3,8 +3,9 @@ from admins import views
 
 urlpatterns = [
   path('', views.adminDashbaord, name='admins'),
-  
-  
-  
-  
+  path('admin_editor', views.admin_editor, name='admin_editor'),
+  path('editor_signUp', views.editor_signUp, name='editor_signUp'),
+
+
+
 ]

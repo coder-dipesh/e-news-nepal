7f7c273be267e0ee3ee1d3b8a3db0572c45d87d9
The-ARJ <joshiaayush871@gmail.com>
2022-05-18 14:24:20 +0545

    Implemented views for Editor dashboard ✔️

--- editors/views.py
+++ editors/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from accounts.auth import editor_only
+
+
+def editorDashboard(request):
+    users = User.objects.all()
+
+    admin_count = users.filter(is_superuser=1).count()
+    editor_count = users.filter(is_superuser=0, is_staff=1).count()
+    user_count = users.filter(is_superuser=0, is_staff=0).count()
+
+    user_info = users.exclude(is_superuser=1)
+
+    context = {'users': users,
+               'editor_count': editor_count,
+               'user_count': user_count,
+               'user_info': user_info,
+
+               }
+    return render(request, 'editors/editorsDashboard.html', context)

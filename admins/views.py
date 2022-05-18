c10f6596b9165afd625c7b272b04b9df7fcb5b7d
The-ARJ <joshiaayush871@gmail.com>
2022-05-18 14:25:54 +0545

    nothing changed here

--- admins/views.py
+++ admins/views.py
@@ -10,7 +10,7 @@ from accounts.auth import admin_only, unauthenticated_user
 from .models import CustomUser
 # Create your views here.
 from accounts.forms import CreateUserForm
-from .forms import CustomUserForm,CreateUserForm
+from .forms import CustomUserForm, CreateUserForm
 from enews import settings
 
 
@@ -18,39 +18,38 @@ from enews import settings
 @admin_only
 def adminDashbaord(request):
     users = User.objects.all()
-    
+
     admin_count = users.filter(is_superuser=1).count()
-    editor_count = users.filter(is_superuser=0,is_staff=1).count()
-    user_count = users.filter(is_superuser=0,is_staff=0).count()
-    
-    user_info= users.exclude(is_superuser=1)
-    
-    
+    editor_count = users.filter(is_superuser=0, is_staff=1).count()
+    user_count = users.filter(is_superuser=0, is_staff=0).count()
+
+    user_info = users.exclude(is_superuser=1)
+
     context = {'users': users,
-                'admin_count': admin_count,
-                'editor_count': editor_count,
-                'user_count': user_count,
-                'user_info': user_info,
-                
-                }
-    return render(request, 'admins/adminDashboard.html',context)
+               'admin_count': admin_count,
+               'editor_count': editor_count,
+               'user_count': user_count,
+               'user_info': user_info,
+
+               }
+    return render(request, 'admins/adminDashboard.html', context)
 
 @login_required
 @admin_only
 def getUsers(request):
     users = User.objects.all()
-    user_info = users.filter(is_superuser=0,is_staff=0)
+    user_info = users.filter(is_superuser=0, is_staff=0)
     context = {'user_info': user_info,
-                'activate_users':'active'}
-    
-    return render(request, 'admins/allUsers.html',context)
+               'activate_users': 'active'}
+
+    return render(request, 'admins/allUsers.html', context)
 
 
 @login_required
 @admin_only
 def getEditor(request):
-    context = {'activate_editors':'active'}
-    return render(request, 'admins/adminEditor/editorPage.html',context)
+    context = {'activate_editors': 'active'}
+    return render(request, 'admins/adminEditor/editorPage.html', context)
 
 
 @login_required
@@ -61,22 +60,21 @@ def editorSignUp(request):
         role = request.POST.get('role')
         salary = request.POST.get('salary')
         if form.is_valid():
-            user= form.save()
+            user = form.save()
             user.save()
             # Getting user and altering its value 
             username = request.POST.get('username')
-            usr = User.objects.get(username = username)
+            usr = User.objects.get(username=username)
             usr.is_staff = True
-            usr.save() # Save user
-            
-            
+            usr.save()  # Save user
+
             if cform.is_valid():
-                CustomUser.objects.create(user=user,role=role,salary=salary)
+                CustomUser.objects.create(user=user, role=role, salary=salary)
             messages.add_message(request, messages.SUCCESS, 'Editor registered successfully!!')
         else:
             messages.add_message(request, messages.ERROR, 'Unable to register editor!!')
-        
+
     context = {'form': CreateUserForm,
-                'cform':CustomUserForm,}
+               'cform': CustomUserForm, }
 
     return render(request, 'admins/adminEditor/registerEditor.html', context)

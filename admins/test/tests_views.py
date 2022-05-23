c4ddab482b20060a66e243aaa2404c6f37668fb3
Bibekbahadurkc <bibekbahadhurkc@gmail.com>
2022-05-23 11:04:59 +0545

    testing

--- admins/test/tests_views.py
+++ admins/test/tests_views.py
@@ -7,3 +7,18 @@ class Test_views(TestCase):
         client = Client()
         response = client.get(reverse('all-editors'))
         self.assertTemplateUsed(response, 'admins/adminEditor/editorPage.html')
+
+    def test_allUsers(self):
+        client = Client()
+        response = client.get(reverse('all-users'))
+        self.assertTemplateUsed(response, 'admins/allUsers.html')
+
+    def test_allCategory(self):
+        client = Client()
+        response = client.get(reverse('all-category'))
+        self.assertTemplateUsed(response, 'admins/Category/categoryPage.html')
+
+    def test_newCategory(self):
+        client = Client()
+        response = client.get(reverse('new-category'))
+        self.assertTemplateUsed(response, 'admins/Category/newCategory.html')

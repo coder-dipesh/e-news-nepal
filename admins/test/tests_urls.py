cf848c9b995688516dff4dfd59c0fa0a979de63c
Bibekbahadurkc <bibekbahadhurkc@gmail.com>
2022-05-23 11:07:48 +0545

    testing urls

--- admins/test/tests_urls.py
+++ admins/test/tests_urls.py
@@ -9,3 +9,24 @@ class TestUrls(SimpleTestCase):
         url = reverse('all-editors')
         view = resolve(url).func
         self.assertEquals(view, getEditor)
+
+    
+
+    # Testing getUsers function via all-users url
+    def test_all_users_urls_is_resolved(self):
+
+        url = reverse('all-users')
+        view = resolve(url).func
+        self.assertEquals(view, getUsers)
+
+    # Testing getCategory function via all-categories url
+    def test_all_category(self):
+        url = reverse('all-category')
+        view = resolve(url).func
+        self.assertEquals(view, getCategory)
+
+    # Testing newCategory function via new-category url testing
+    def test_new_category(self):
+        url = reverse('new-category')
+        view = resolve(url).func
+        self.assertEquals(view, newCategory)

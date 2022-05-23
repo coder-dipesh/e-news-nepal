d3d197fa2c52dad13eb271f677d0cceffc7b20ed
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-05-23 11:47:54 +0545

    form test

--- admins/test/tests_forms.py
+++ admins/test/tests_forms.py
@@ -1,3 +1,17 @@
 from django.test import SimpleTestCase
 from admins.forms import *
 
+class TestForms(SimpleTestCase):
+    def test_category_form(self):
+        form = CategoryForm(data={
+            'title': 'Sports',
+            'categoryName': 'Football',
+            'description': "All about the sports and football",
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_category_form(self):
+        form = CategoryForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 3)

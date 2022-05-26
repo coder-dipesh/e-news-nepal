267b791df112fdd46d99222027d9fb81852d1231
The-ARJ <joshiaayush871@gmail.com>
2022-05-26 17:25:02 +0545

    🅰️ views file implemented for news report  ✔️

--- users/views.py
+++ users/views.py
@@ -7,18 +7,27 @@ from users.models import ReportNewsModel
 
 
 def report_news(request):
-    con = {'success': False}
-    # user = request.user.id
-    # form = ReportNewsForm(request.POST, request.FILES)
+    form = ReportNewsForm(request.POST, request.FILES)
     if request.method == "POST":
-        print(request.POST)
-        form = ReportNewsForm(request.POST, request.FILES)
-        form.save()
-        print("Item created")
+        # print(request.POST)
+        # form = ReportNewsForm(request.POST, request.FILES)
+        # form.save()
+        # print("Item created")
 
+        if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            contact = request.POST['contact']
+            category = request.POST['category']
+            content = request.POST['content']
+            image = request.FILES['image']
+
+            ReportNewsModel.objects.create(
+                name=name, category_id=category, email=email, content=content, contact=contact,
+                image=image)
         messages.success(request,"News Reported Successfully!!")
+        return redirect('/users/report_news')
+
     context = {'form': ReportNewsForm,
                'activate_report_news': 'active'}
-
-
     return render(request, 'users/reportnews.html', context)

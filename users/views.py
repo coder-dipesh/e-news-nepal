bdba2de4dcc4f436e68ebe477c6404707a53be0e
The-ARJ <joshiaayush871@gmail.com>
2022-05-26 14:53:57 +0545

    views file implemented 🅰️✔️

--- users/views.py
+++ users/views.py
@@ -7,24 +7,18 @@ from users.models import ReportNewsModel
 
 
 def report_news(request):
-    user = request.user.id
-    form = ReportNewsForm(request.POST, request.FILES)
-    if request.method == 'POST':
-        if form.is_valid():
-            rname = request.POST['rname']
-            remail = request.POST['remail']
-            rcontact = request.POST['rcontact']
-            rcategory = request.POST['rcategory']
-            rcontent = request.POST['rcontent']
-            rimage = request.FILES['rimage']
+    con = {'success': False}
+    # user = request.user.id
+    # form = ReportNewsForm(request.POST, request.FILES)
+    if request.method == "POST":
+        print(request.POST)
+        form = ReportNewsForm(request.POST, request.FILES)
+        form.save()
+        print("Item created")
 
-            ReportNewsModel.objects.create(
-                rname=rname, rcategory_id=rcategory, remail=remail, rcontent=rcontent, rcontact=rcontact,
-                rimage=rimage, ruser_id=user)
-            messages.add_message(request, messages.SUCCESS,
-                                 'News Reported Successfully!')
-            return redirect('/users/report_news')
-
+        messages.success(request,"News Reported Successfully!!")
     context = {'form': ReportNewsForm,
                'activate_report_news': 'active'}
+
+
     return render(request, 'users/reportnews.html', context)

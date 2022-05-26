aba58ef53307a29cf29672294a3e8e4aab29af23
The-ARJ <joshiaayush871@gmail.com>
2022-05-26 13:17:11 +0545

    users views implemented✔️

--- users/views.py
+++ users/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from users.forms import ReportNewsForm
+from users.models import ReportNewsModel
+
+
 def report_news(request):
-    return render(request, 'users/reportnews.html')
+    user = request.user.id
+    form = ReportNewsForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        if form.is_valid():
+            rname = request.POST['rname']
+            remail = request.POST['remail']
+            rcontact = request.POST['rcontact']
+            rcategory = request.POST['rcategory']
+            rcontent = request.POST['rcontent']
+            rimage = request.FILES['rimage']
+
+            ReportNewsModel.objects.create(
+                rname=rname, rcategory_id=rcategory, remail=remail, rcontent=rcontent, rcontact=rcontact,
+                rimage=rimage, ruser_id=user)
+            messages.add_message(request, messages.SUCCESS,
+                                 'News Reported Successfully!')
+            return redirect('/users/report_news')
+
+    context = {'form': ReportNewsForm,
+               'activate_report_news': 'active'}
+    return render(request, 'users/reportnews.html', context)

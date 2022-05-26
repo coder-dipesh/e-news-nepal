f805d2658342d8d47db2d06f16accbfcf7911164
The-ARJ <joshiaayush871@gmail.com>
2022-05-26 13:15:12 +0545

    admin editor js implemented ✔️

--- static/js/admin_editor.js
+++ static/js/admin_editor.js
@@ -1,19 +1,23 @@
 let noOfCharac = 150;
 let contents = document.querySelectorAll(".content");
 contents.forEach((content) => {
-  //If text length is less that noOfCharac... then hide the read more button
-  if (content.textContent.length < noOfCharac) {
-    content.nextElementSibling.style.display = "none";
-  } else {
-    let displayText = content.textContent.slice(0, noOfCharac);
-    let moreText = content.textContent.slice(noOfCharac);
-    content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
-  }
+    //If text length is less that noOfCharac... then hide the read more button
+    if (content.textContent.length < noOfCharac) {
+        content.nextElementSibling.style.display = "none";
+    } else {
+        let displayText = content.textContent.slice(0, noOfCharac);
+        let moreText = content.textContent.slice(noOfCharac);
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
 });
 
 function readMore(btn) {
-  let post = btn.parentElement;
-  post.querySelector(".dots").classList.toggle("hide");
-  post.querySelector(".more").classList.toggle("hide");
-  btn.textContent == "View More" ? (btn.textContent = "View Less") : (btn.textContent = "View More");
+    let post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "View More" ? (btn.textContent = "View Less") : (btn.textContent = "View More");
+}
+
+if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
 }

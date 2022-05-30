82e48c8a952ba9137f14bf9b98486a3f5fba017f
Aabhas1996 <aabhastinkari9@gmail.com>
2022-05-30 13:54:05 +0545

    This is the url testing of Editor part done by aabash

---
+++ editors/test/tests_urls.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from editors.views import *
+from django.urls import reverse, resolve
+
+
+class TestUrls(SimpleTestCase) :
+     def test_my_news_urls_is_resolved(self):
+         url = reverse('my-news')
+         view = resolve(url).func
+         self.assertEquals(view, myNews)

371d4b2432765fc0408841226d6ca5b2a13def66
itsmeshishir <motivate23a@gmail.com>
2022-06-01 05:55:15 +0545

    This is just the little bit of css i forget to push so sorry for that

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from users import views
 
 urlpatterns = [
-    path('report_news', views.report_news, name='report_news'),
+    # path('report_news', views.report_news, name='report_news'),
     path('view_news', views.view_news, name='view_news'),
     path('about_us', views.about_us, name='about_us'),
 ]

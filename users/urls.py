4a635d11dae36fd327479289a2135b8469fe30ee
The-ARJ <joshiaayush871@gmail.com>
2022-06-01 21:33:04 +0545

    🅰️ minor fix ✔️

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from users import views
 
 urlpatterns = [
     path('report_news', views.report_news, name='report_news'),
-    path('view_news', views.view_news, name='view_news'),
     path('about_us', views.about_us, name='about_us'),
     path('contact_us', views.contact_us, name='contact_us'),
     path('account', views.account, name='account'),
--- users/views.py
+++ users/views.py
@@ -32,26 +32,6 @@ def report_news(request):
     return render(request, 'users/reportnews.html', context)
 
 
-def view_news(request):
-    # form = ReportNewsForm(request.POST, request.FILES)
-    # if request.method == "POST":
-    # if form.is_valid():
-    #     title = request.POST['title']
-    #     name = request.POST['name']
-    #     email = request.POST['email']
-    #     contact = request.POST['contact']
-    #     category = request.POST['category']
-    #     content = request.POST['content']
-    #     image = request.FILES['image']
-    #
-    #     NewsModel.objects.create(
-    #         name=name, email=email, contact=contact, title=title, category_id=category, content=content, image=image)
-    #     return redirect('/users/report_news')
-    #
-    # context = {'form': ReportNewsForm,
-    #            'activate_report_news': 'active'}
-    return render(request, 'users/viewnews.html')
-
 
 def about_us(request):
     return render(request, 'users/aboutus.html')

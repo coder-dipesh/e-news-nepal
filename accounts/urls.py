43efd0636b337c2eecb2ee1be69613472f1899c1
itsmeshishir <motivate23a@gmail.com>
2022-06-01 05:48:07 +0545

    this is the view news done shishir user can go to the home page and view neews page location is account/template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,18 @@
 from accounts import views
-from django.urls import  path,include
+from django.urls import path, include
 
 urlpatterns = [
-  # Socail Auth 
-  path('',include('django.contrib.auth.urls')),
+    # Socail Auth
+    path('', include('django.contrib.auth.urls')),
 
-  
-  # Login Register
-  path('sign-in', views.signIn, name='sign-in'),
-  path('sign-up', views.signUp, name='sign-up'),
-  path('sign-out', views.signOut, name='sign-out'),
-  
-  path('', views.home, name='home'),
-  
-  
+
+    # Login Register
+    path('sign-in', views.signIn, name='sign-in'),
+    path('sign-up', views.signUp, name='sign-up'),
+    path('sign-out', views.signOut, name='sign-out'),
+    path('viewnews/<int:news_id>', views.viewnews, name='viewnews'),
+    
+    path('', views.home, name='home'),
+
+
 ]

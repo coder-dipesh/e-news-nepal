2f811ca5f352d823cd9a2e3fe6073f41612945d8
coder-dipesh <dipeshsiwakoti112@gmail.com>
2022-06-02 11:05:46 +0545

    cleaning code

--- static/js/lcs.js
+++ static/js/lcs.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(document).one('click', '.like-review', function(e) {
-		$(this).html('<i class="fa fa-thumbs-up" aria-hidden="true"></i>liked');
+		$(this).html('<i class="fa fa-thumbs-up" aria-hidden="true"></i>Liked');
 		$(this).children('.fa-thumbs-up').addClass('animate-like');
 	});
 });

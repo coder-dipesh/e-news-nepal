d8944240545b10fafcef786764b1f337dd436586
Aabhas1996 <aabhastinkari9@gmail.com>
2022-06-06 09:39:25 +0545

    test view completed

--- users/test/tests_views.py
+++ users/test/tests_views.py
@@ -8,3 +8,12 @@ class Test_views(TestCase):
         response = client.get(reverse('report_news'))
         self.assertTemplateUsed(response, 'users/reportnews.html')
         
+        def test_about_us(self):
+            client = Client()
+        response = client.get(reverse('about_us'))
+        self.assertTemplateUsed(response, 'users/aboutus.html')
+
+    def test_contact_us(self):
+        client = Client()
+        response = client.get(reverse('contact_us'))
+        self.assertTemplateUsed(response, 'users/contactus.html')

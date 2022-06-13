ce4d6a15b1a057850c4784838f40e900e21f472d
Bibekbahadurkc <bibekbahadhurkc@gmail.com>
2022-06-13 09:31:19 +0545

    Users views testing

--- users/test/tests_views.py
+++ users/test/tests_views.py
@@ -1,2 +1,13 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+
+class Test_views(TestCase):
+    def test_about(self):
+        client = Client()
+        response = client.get(reverse('about_us'))
+        self.assertTemplateUsed(response, 'users/aboutus.html')
+
+    def test_contact(self):
+        client = Client()
+        response = client.get(reverse('contact_us'))
+        self.assertTemplateUsed(response, 'users/contactus.html')

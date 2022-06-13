6af24e099e9e0f514613820470d55fa1aa53d828
Bibekbahadurkc <bibekbahadhurkc@gmail.com>
2022-06-13 09:35:16 +0545

    url testing

--- users/test/tests_urls.py
+++ users/test/tests_urls.py
@@ -1,3 +1,9 @@
 from django.test import SimpleTestCase
 from users.views import *
 from django.urls import reverse, resolve
+
+class TestUrls(SimpleTestCase):
+    def test_urls_about(self):
+        url = reverse('about_us')
+        view = resolve(url).func
+        self.assertEquals(view, about_us)
